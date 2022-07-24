9b61266f7ae61c2f438d4efcf8ae5ca95b79c4b7
leejae honey <ljh23270@naver.com>
2022-07-24 21:49:30 +0900

    chat form

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import http from "http";
 import WebSocket from "ws";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
@@ -16,6 +16,26 @@ app.get("/*", (req, res) => {
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
